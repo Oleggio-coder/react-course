22bf888dd05b34c6869a23ff2d4fdecd1c6840bf
Oleh Seredenko <olezhkaseredenko.190693@gmail.com>
2023-03-21 17:06:24 +0200

    getting started commit

---
+++ getting-started/lesson-4/react-app/src/Message.tsx
@@ -0,0 +1,18 @@
+
+// PascalCasing
+
+function Message() {
+
+  //JSX: JavaScriptXML
+
+  const name = 'Oleg';
+
+  if (name) {
+    return <h1>Hello, {name}!</h1>
+  } else {
+    return <h1>Hello, World!</h1>
+  }
+
+}
+
+export default Message;
